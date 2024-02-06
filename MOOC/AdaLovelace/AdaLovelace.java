949c940d52eac54f17836f33756c88ed3acc84fb
ryanrmills <ryanroymills@gmail.com>
2024-02-06 13:46:42 -0800

    adding Ada Lovelace

---
+++ MOOC/AdaLovelace/AdaLovelace.java
@@ -0,0 +1,8 @@
+package alwayslearning.MOOC.AdaLovelace;
+
+public class AdaLovelace {
+    public static void main (String[] args) {
+        //Write the program here
+        System.out.println("Ada Lovelace");
+        }
+    } 
