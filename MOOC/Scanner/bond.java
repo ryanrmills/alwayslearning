1b851688fc668706cbf24324e718f0f3b64b9f73
ryanrmills <ryanroymills@gmail.com>
2024-02-06 19:27:42 -0800

    My name is Bond, James Bond

---
+++ MOOC/Scanner/bond.java
@@ -0,0 +1,8 @@
+public class bond {
+    public static void main(String[] args) {
+        String start = "My name is ";
+        String end = ", James Bond.";
+
+        System.out.println(start + "Bond" + end);
+    }
+}
